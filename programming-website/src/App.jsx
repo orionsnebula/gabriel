8a6f694f927e6a818a1360cdb6b3a8410e0c7cee
deniaverkade <galgoboulevard@gmail.com>
2026-02-06 16:57:50 +0100

    project init

---
+++ programming-website/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <img src="../images/placeholder.png" />
+    <h2>my friend my enemy get off the website its not finished</h2>
+    </>
+  )
+}
+
+export default App
